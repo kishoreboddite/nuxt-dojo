4ec48eaed5ec06bb7759139dd7beb120fd26685f
kishoreboddite <kishoreboddite416@gmail.com>
2023-02-01 13:09:38 +0530

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse totam cupiditate laudantium quasi velit, rerum enim accusantium, aspernatur minima saepe doloremque, eaque quaerat asperiores? Alias dolorum obcaecati repellat voluptatem?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quidem, distinctio labore rerum totam doloremque soluta ullam voluptatem rem nulla, quos quam eaque esse iusto commodi neque ea quisquam laborum?</p>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+h2{
+    margin-bottom: 20PX;
+    font-size: 36PX;
+}
+p{
+    margin: 20px 0;
+}
+</style>
